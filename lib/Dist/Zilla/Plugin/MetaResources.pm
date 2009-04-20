530b0d31be656c54fb5b03db5ad2ff86ea925c74
Fayland 林 <fayland@gmail.com>
2009-04-20 13:50:31 +0000

    Import of FAYLAND/Dist-Zilla-Plugin-MetaResources-0.02 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-MetaResources
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         FAYLAND/Dist-Zilla-Plugin-MetaResources-0.02.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/MetaResources.pm
+++ lib/Dist/Zilla/Plugin/MetaResources.pm
@@ -9,6 +9,7 @@ with 'Dist::Zilla::Role::InstallTool';
 use Dist::Zilla::File::InMemory;
 
 has 'homepage';
+has 'license';
 has 'repository';
 has 'bugtracker';
 has 'MailingList';
@@ -17,15 +18,46 @@ sub setup_installer {
     my ( $self, $arg ) = @_;
 
     my $has_meta;
-    foreach ( 'homepage', 'repository', 'bugtracker', 'MailingList' ) {
+    foreach ( 'homepage', 'license', 'repository', 'bugtracker', 'MailingList' )
+    {
         $has_meta++ if $self->{$_};
     }
     return unless $has_meta;
 
+    # check if [MetaYaml] is there
+    my $has_metayml = $self->zilla->plugins->grep(
+        sub { ref $_ eq 'Dist::Zilla::Plugin::MetaYaml' } )->length;
+    if ($has_metayml) {
+
+        # get orignal Makefile.PL
+        my $file =
+          $self->zilla->files->grep( sub { $_->name =~ m{META\.yml\z} } )->head;
+
+        if ($file) {
+            my $content = $file->content;
+            require YAML::Syck;
+            my $meta = YAML::Syck::Load($content);
+            foreach (
+                'homepage',   'license', 'repository',
+                'bugtracker', 'MailingList'
+              )
+            {
+                if ( $self->{$_} ) {
+                    $meta->{resources}{$_} = $self->{$_};
+                }
+            }
+            $file->content( YAML::Syck::Dump($meta) );
+        }
+        else {
+            $self->zilla->log(
+"[MetaResources] Skip META.yml ([MetaResources] needs after [MetaYaml]"
+            );
+        }
+    }
+
     # check if [MakeMaker] is there
     my $has_maker = $self->zilla->plugins->grep(
         sub { ref $_ eq 'Dist::Zilla::Plugin::MakeMaker' } )->length;
-
     if ($has_maker) {
 
         # get orignal Makefile.PL
@@ -40,7 +72,11 @@ sub setup_installer {
   META_MERGE  => {
     resources => {
 ~;
-            foreach ( 'homepage', 'repository', 'bugtracker', 'MailingList' ) {
+            foreach (
+                'homepage',   'license', 'repository',
+                'bugtracker', 'MailingList'
+              )
+            {
                 if ( $self->{$_} ) {
                     $meta_resources .= "        $_ => '$self->{$_}',\n";
                 }
@@ -56,9 +92,9 @@ sub setup_installer {
         }
     }
 
+    # check ModuleBuild
     my $has_build = $self->zilla->plugins->grep(
         sub { ref $_ eq 'Dist::Zilla::Plugin::ModuleBuild' } )->length;
-
     if ($has_build) {
 
         # get orignal Makefile.PL
@@ -105,7 +141,7 @@ Dist::Zilla::Plugin::MetaResources - Dist::Zilla with meta resources
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 NAME
 
@@ -113,7 +149,7 @@ Dist::Zilla::Plugin::MetaResources - Dist::Zilla with meta resources
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
@@ -121,8 +157,10 @@ version 0.01
 
       [MetaResources]
       repository = http://github.com/fayland/dist-zilla-plugin-metaresources/tree/master
+      license = http://dev.perl.org/licenses/
       homepage = http://github.com/fayland/dist-zilla-plugin-metaresources
       bugtracker = http://github.com/fayland/dist-zilla-plugin-metaresources/issues
+      MailingList = somewhere@here.com
 
 =head1 DESCRIPTION
 
